0ba24e8c4a60c9ae0dd10d1dc8354d140e332759
rodrigo <rodrigonehring@hotmail.com>
2017-08-22 15:42:54 -0300

    init

---
+++ app/helpers/sse.js
@@ -0,0 +1,79 @@
+const sseConfig = {
+  'Content-Type': 'text/event-stream',  // <- Important headers
+  'Cache-Control': 'no-cache',
+  'Connection': 'keep-alive',
+  'Transfer-Encoding': true,
+};
+
+function mountWhere(prefix, params = {}) {
+  if (!prefix) {
+    throw new Error('Must insert prefix to mount where');
+  }
+
+  const where = Object.keys(params)
+    .map(param => `[${param}]:${params[param]}`)
+    .join(':');
+
+  return where ? `${prefix}-${where}` : prefix;
+}
+
+class SSE {
+  constructor() {
+    this.currentIndex = 0;
+    this.clients = {}; // <- Keep a map of attached clients
+  }
+
+  // pattern: arrayOf ids
+  route({ params, prefix }, cb) {
+    return (req, res) => {
+      req.socket.setTimeout(Number.MAX_VALUE);
+      res.writeHead(200, sseConfig);
+      res.write('\n');
+
+      const paramsObj = params && params.reduce((acc, curr) => {
+        acc[curr] = req.params[curr];
+
+        return acc;
+      }, {});
+
+      const where = mountWhere(prefix, paramsObj);
+
+      const id = this.currentIndex++;
+
+      if (this.clients[where]) {
+        this.clients[where][id] = res;
+      } else {
+        this.clients[where] = { [id]: res };
+      }
+
+      req.on('close', () => { delete this.clients[where][id] });
+
+      cb && cb(req, res);
+    }
+  }
+
+  // config: { data: {}, params: {} }
+  emitTo({ prefix, params, data, event }) {
+    const where = mountWhere(prefix, params);
+
+    if (!this.clients[where]) {
+      return console.log('No clients for: ', where);
+    }
+
+    const str = `data: ${JSON.stringify({ event, data })} \n\n`;
+
+    Object
+      .keys(this.clients[where])
+      .forEach(id =>
+        this.clients[where][id].write(str)
+      );
+  }
+
+  clientsConnected({ prefix, params }) {
+    const where = mountWhere(prefix, params);
+
+    return this.clients[where] ? Object.keys(this.clients[where]).length : 0;
+  }
+}
+
+module.exports = SSE;
---
+++ app/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const SSE = require('./helpers/sse');
+const sse = new SSE();
+
+const ENV = 'development';
+const port = process.env.PORT || 8080;
+
+// parse application/json
+app.use(bodyParser.json());
+
+// template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', function(req, res) {
+  res.render('index')
+});
+
+// app.get('/sse/item/:itemId/:userId', sse.route({
+//   params: ['itemId', 'userId'],
+//   prefix: 'items'
+// }));
+
+app.get('/sse/all', sse.route({
+  prefix: 'all'
+}, (req, res) => {
+  const sendEvent = () => {
+    sse.emitTo({
+      prefix: 'all',
+      event: 'user_connection',
+      data: { total: sse.clientsConnected({ prefix: 'all' }) }
+    });
+  }
+
+  sendEvent();
+
+  req.on('close', () => {
+    sendEvent();
+  });
+}));
+
+// setInterval(() => {
+//   sse.emitTo({
+//     prefix: 'all',
+//     data: { message: 'eai' }
+//   });
+
+//   sse.emitTo({
+//     prefix: 'al321321l',
+//     data: { message: 'eai' }
+//   });
+// }, 2000);
+
+app.listen(port);
+
+console.log(`App running on port: ${port} in ${ENV} mode`);
